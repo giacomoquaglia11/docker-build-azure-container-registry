7fb10c6894644cef66fc3f75acf69f21cc7caa50
giacomoquaglia11 <153645847+giacomoquaglia11@users.noreply.github.com>
2023-12-13 18:44:17 +0100

    Add files via upload

---
+++ docker-registry/variables.tf
@@ -0,0 +1,9 @@
+# Credentials Azure
+variable "account_azure_subscription_id" {}
+variable "account_azure_client_id" {}
+variable "account_azure_client_secret" {}
+variable "account_azure_tenant_id" {}
+
+# General Variables
+variable "resource_group_name" { default = "Giacomo-Quaglia-001" }
+variable "region" { default = "North Europe" }
